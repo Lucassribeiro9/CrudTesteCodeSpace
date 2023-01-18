cc3a99fad59cc7204fcddebf87b03efbdba7a022
Lucassribeiro9 <57766036+Lucassribeiro9@users.noreply.github.com>
2023-01-18 18:53:16 +0000

    crud mvc vscode

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CrudTesteCodeSpace.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
